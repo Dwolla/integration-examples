ca655578e6321ef6a9a8f133b2022d747d709455
James Coon <james@jcoon.dev>
2022-06-08 16:22:41 -0400

    DEV-921: Add integration application for Dwolla/Plaid Funding Source creation (#1)
    * Add integration application for Dwolla/Plaid Funding Source creation
    
    * Default Dwolla and Plaid environments to sandbox

---
+++ dwolla-plaid-funding-source/pages/api/plaid/create-link-token.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { createLinkToken } from "../../../app/plaid";
+
+export default async function createLinkTokenApi(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "POST") {
+        const linkToken = await createLinkToken();
+        res.status(200).json(linkToken);
+    } else {
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
